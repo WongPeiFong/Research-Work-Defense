8a12d778061e9e0fc35a4329729b33135a83cb39
WongPeiFong <161627250+WongPeiFong@users.noreply.github.com>
2025-03-05 18:12:03 +0300

    Update MHC I.R

--- MHC I.R
+++ MHC I.R
@@ -2,8 +2,10 @@ library(netmhcpan)
 fasta_filename <- system.file(
   "extdata", "example.fasta", package = "netmhcpan"
 )
+# NetMHCIIpan supports more than 5000 alleles:
 all_alleles <- get_netmhcpan_alleles()
 testit::assert(length(all_alleles) > 5000)
+# select alleles
 alleles <- all_alleles[1:300]
 bindings <- run_netmhcpan(
   fasta_filename = fasta_filename,
