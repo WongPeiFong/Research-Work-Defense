ec4f8be33eeeea3a9c2d8cc74c30702a05429975
WongPeiFong <161627250+WongPeiFong@users.noreply.github.com>
2025-03-05 17:47:54 +0300

    Create MHC I.R

---
+++ MHC I.R
@@ -0,0 +1,15 @@
+tar -xvzf netMHCpan-4.1.Linux.tar.gz
+cd netMHCpan-4.1
+chmod +x netMHCpan
+
+fasta_filename <- system.file(
+  "extdata", "example.fasta", package = "netmhcpan"
+)
+all_alleles <- get_netmhcpan_alleles()
+testit::assert(length(all_alleles) > 5000)
+alleles <- all_alleles[1:300]
+bindings <- run_netmhcpan(
+  fasta_filename = fasta_filename,
+  alleles = alleles
+)
+knitr::kable(bindings)
